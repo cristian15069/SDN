24f6212e37a9797368182fb4b2ca7a2c3cac5bb4
Cristian Garcia <cristiang150404@gmail.com>
2025-11-11 22:46:33 -0600

    Update functions of zombies attack and player

--- Assets/AnimationEventBridge.cs
+++ Assets/AnimationEventBridge.cs
@@ -6,18 +6,13 @@ public class AnimationEventBridge : MonoBehaviour
 
     public void DealDamageFromAnimation()
     {
-        // TRAMPA 1: ¿Se está llamando esta función?
-        Debug.Log("--- 1. Evento 'DealDamage' RECIBIDO por el 'Bridge' (Hijo) ---");
 
         if (zombieController != null)
         {
-            // TRAMPA 2: ¿Está conectado al Padre?
-            Debug.Log("--- 2. 'Bridge' está conectado. Pasando mensaje al 'Padre' (ZombieController)... ---");
             zombieController.DealDamageFromAnimation();
         }
         else
         {
-            // TRAP 2 (FALLO): ¡No está conectado!
             Debug.LogError("¡ERROR DE PUENTE! El 'Animator' (Hijo) disparó el evento, pero la casilla 'Zombie Controller' en 'AnimationEventBridge' (Hijo) está VACÍA.");
         }
     }
